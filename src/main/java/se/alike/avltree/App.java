576d39ff3abc97f4c43b2fcdd35a6173649b386c
roku6185 <robin@alike.se>
2018-01-06 17:55:32 +0100

    Add maven directory structure

---
+++ src/main/java/se/alike/avltree/App.java
@@ -0,0 +1,13 @@
+package se.alike.avltree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
