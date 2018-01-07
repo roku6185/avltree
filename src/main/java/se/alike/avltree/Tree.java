2c5d3db9c16fd85b2572b572e62f14b760a0bd81
roku6185 <robin@alike.se>
2018-01-07 07:10:09 +0100

    Add interface for trees

---
+++ src/main/java/se/alike/avltree/Tree.java
@@ -0,0 +1,11 @@
+package se.alike.avltree;
+
+public interface Tree<T>
+{
+  T getObject();
+  Tree<T> getLeft();
+  Tree<T> getRight();
+  void insert(T object);
+  void delete(T object);
+  T search(T object);
+}
