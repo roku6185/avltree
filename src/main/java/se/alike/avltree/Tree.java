0d40c07db6ea6446d76e1fc13abe1d25d68e62bb
roku6185 <robin@alike.se>
2018-01-10 23:53:28 +0100

    Cleanup Tree interface

--- src/main/java/se/alike/avltree/Tree.java
+++ src/main/java/se/alike/avltree/Tree.java
@@ -2,9 +2,6 @@ package se.alike.avltree;
 
 public interface Tree<T>
 {
-  T getObject();
-  Tree<T> getLeft();
-  Tree<T> getRight();
   void insert(T object);
   void delete(T object);
   boolean search(T object);
