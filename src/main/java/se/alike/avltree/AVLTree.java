cf18b99a601cf75877b286cfa19f9b2429e1b6fc
roku6185 <robin@alike.se>
2018-01-21 18:02:07 +0100

    Add comment to functions

--- src/main/java/se/alike/avltree/AVLTree.java
+++ src/main/java/se/alike/avltree/AVLTree.java
@@ -9,6 +9,11 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
   private T object;
   private int height;
 
+  /**
+   * Creates an empty AVL tree.
+   *
+   * @return AVLTree<T>
+   */
   public AVLTree()
   {
     this.left = null;
@@ -17,29 +22,56 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     this.height = 0;
   }
 
+  /**
+   * Returns the object of the root node of this tree.
+   *
+   * @return T
+   */
   @Override
   public T getObject()
   {
     return object;
   }
 
+  /**
+   * Returns the left subtree of this tree.
+   *
+   * @return AVLTree<T>
+   */
   @Override
   public AVLTree<T> getLeft()
   {
     return left;
   }
 
+  /**
+   * Returns the right subtree of this tree.
+   *
+   * @return AVLTree<T>
+   */
   @Override
   public AVLTree<T> getRight()
   {
     return right;
   }
 
+  /**
+   * Returns the height of this tree.
+   * A node with a single node has a height of 1.
+   *
+   * @return AVLTree<T>
+   */
   public int getHeight()
   {
     return height;
   }
 
+  /**
+   * Returns the balance factor of this tree, which is defined as:
+   * height(left subtree) - height(right subtree))
+   *
+   * @return int
+   */
   private int getBalanceFactor()
   {
     int leftHeight = getLeft() == null ? 0 : getLeft().height;
@@ -47,6 +79,13 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     return leftHeight - rightHeight;
   }
 
+  /**
+   * Performs an auto-balanced insert into this tree.
+   * An object less than root node will ended up in left subtree,
+   * otherwise in the right subtree.
+   *
+   * @param other Object to insert into this tree
+   */
   @Override
   public void insert(T other)
   {
@@ -101,7 +140,8 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     }
   }
 
-  /* Left rotation:
+  /**
+   * Performs a left rotation:
    *
    *    1              2
    *   / \            / \
@@ -132,7 +172,8 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     );
   }
 
-  /* Right rotation:
+  /**
+   * Performs a right rotation:
    *
    *      3          2
    *     / \        / \
@@ -163,12 +204,25 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     );
   }
 
+  /**
+   * Deletes an object from this tree.
+   *
+   * @param other Object to delete
+   */
   @Override
   public void delete(T other)
   {
     delete(other, this, null);
   }
 
+  /**
+   * Recursively deletes an object <other> from tree <root> with 
+   * parent tree <parent>.
+   *
+   * @param other Object to delete
+   * @param root Tree to delete object from
+   * @param parent Parent of <root>
+   */
   private void delete(T other, AVLTree<T> root, AVLTree<T> parent)
   {
     if (root.object.compareTo(other) == 0)
@@ -179,6 +233,11 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
       delete(other, root.right, root);
   }
 
+  /**
+   *
+   * @param node Node containing object to delete
+   * @param parent Parent of <node>
+   */
   private void removeNode(AVLTree<T> node, AVLTree<T> parent)
   {
     // Case 1: Two children
@@ -202,6 +261,12 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     }
   }
 
+  /**
+   * Disconnects and returns the minimum object from the right subtree 
+   * of the tree <root>.
+   *
+   * @param root
+   */
   private T disconnectMinRight(AVLTree<T> root)
   {
     // Find minimum node in right subtree
@@ -222,6 +287,12 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     return root.object;
   }
 
+  /**
+   * Searches for an object in this tree. Returns true if object 
+   * was found, otherwise false.
+   *
+   * @return boolean
+   */
   @Override
   public boolean search(T other)
   {
@@ -238,6 +309,12 @@ public class AVLTree<T extends Comparable<? super T>> implements BinaryTree<T>
     return false;
   }
 
+  /**
+   * Returns a string representation of this tree.
+   * Used for debug purposes.
+   *
+   * @return string
+   */
   @Override
   public String toString()
   {
